628d9444ce5d28f46009718ad92282d2a24dfb85
Kerem <keremakillioglu9@gmail.com>
2021-01-15 19:45:51 +0300

    iniated api code

---
+++ api/account.go
@@ -0,0 +1,114 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	db "github.com/keremakillioglu/simplebank/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+// balance is zero initially
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR TRY"`
+}
+
+// in gin, everything we do includes a context object
+// handlerFunc  involves *Context input
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	// if client provided invalid data
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		// param1:httpcode :send 400 bad request, param2: httpobject to send to client
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	//if no errors
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Balance:  0,
+		Currency: req.Currency,
+	}
+
+	// return created account in db & error
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		// internal error (code 500), error message
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// successfully created the account
+	ctx.JSON(http.StatusOK, account)
+
+}
+
+type getAccountRequest struct {
+	ID      int64 `uri:"id" binding:"required,min=1"`
+	Balance int64 `json:"balance"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	// if client provided invalid data
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		// param1:httpcode :send 400 bad request, param2: httpobject to send to client
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		// internal error (code 500), error message
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+
+}
+
+type listAccountRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listAccount(ctx *gin.Context) {
+	var req listAccountRequest
+	// if client provided invalid data
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	// limit= pagesize, offset= number of records that db should skip
+	arg := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	account, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
+
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		// internal error (code 500), error message
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+
+}
---
+++ api/server.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/keremakillioglu/simplebank/db/sqlc"
+)
+
+// Server serves HTTP requests for our banking service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates  a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// route, handlerfunc
+	//if we pass multiple parameters: route,middlewares, handlefunc
+	router.POST("/accounts", server.createAccount)
+
+	// id is a parameter provided by URI
+	router.GET("/accounts/:id", server.getAccount)
+
+	// parameters will be retrieved from querystring
+	router.GET("/accounts", server.listAccount)
+
+	// add routes to router
+	server.router = router
+	return server
+}
+
+// we had to get access to store object to set new account to database
+
+// Start runs the HTTP Server on a specific address
+// take an address as an input and return an error
+func (server *Server) Start(address string) error {
+	// use the action provided by gin, sinc erouter part is private it cannot be accessed from outside of this api package
+	return server.router.Run(address)
+}
+
+// returns key-value pair
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
